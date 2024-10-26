126e133a4b544752ad534c670929d848b46514cf
Mariano369 <104920437+Mariano369@users.noreply.github.com>
2024-10-25 21:25:02 -0300

    SISTEMA DE FACTURACION

--- .history/C8 Objects + Callbacks/callbacks/ejercicios/02_20240814202051.js
+++
@@ -1,9 +0,0 @@
-function cambiarCadena(string, callback) {
-   // Aplica la función de callback al string y devuelve el resultado.
-   // La función de callback se encargará de recibir el string y devolverlo con los cambios.
-   // Si no se recibe una función callback entonces se debe retornar el string original.
-   // Tu código:
-  var resultado =  cambiarCadena(cb)
-}
-
-module.exports = cambiarCadena;
